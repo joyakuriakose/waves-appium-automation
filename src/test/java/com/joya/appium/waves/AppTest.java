904f90133cb7c857c86844233125b1193dad9ffc
Joya Kuriakose <joyakuriakose@gmail.com>
2026-03-02 11:13:43 +0530

    waves auto appium first commit

---
+++ src/test/java/com/joya/appium/waves/AppTest.java
@@ -0,0 +1,15 @@
+package com.joya.appium.waves;
+
+import org.testng.annotations.Test;
+
+/**
+ * Placeholder test class (auto-generated, safe to ignore).
+ */
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        // Placeholder - actual tests are in waves_first.MyNewWaves
+        assert true;
+    }
+}
